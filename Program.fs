6341bf8de7f46b179084e7ee1b2e3d5be4b14f68
Vriddhi Pai <49198210+vriddhipai@users.noreply.github.com>
2020-09-21 14:12:12 -0400

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿// // Learn more about F# at http://fsharp.org
+
+// open System
+
+// [<EntryPoint>]
+// let main argv =
+//     printfn "Hello World from F#!"
+//     0 // return an integer exit code
+
+module HelloSquare
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv =
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // Return an integer exit code
